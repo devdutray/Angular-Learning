a6e380ba1ca37c7cd324a7f95b38f62c055c3f01
Debdut Roy <Debdut.Roy@hyland.com>
2023-12-12 13:55:32 +0530

    Data Binding and basic Directives

--- Angular Basics/src/app/servers/servers.component.ts
+++ Angular Basics/src/app/servers/servers.component.ts
@@ -2,9 +2,38 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
+  
+  allowNewServer = false;
+  serverCreationStatus = "No new servers";
+  serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['Test Server 1', 'Test Server 2'];
 
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'New Server Created! Name is ' + this.serverName;
+    console.log(this.serverCreationStatus);
+    // let divElement = document.getElementById("appendHere");
+    // const newServer = document.createElement("app-server");
+    // divElement.appendChild(newServer);
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
